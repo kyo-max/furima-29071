4f62d3a04dd3e357b8ca5f776529d7e9f9e34496
kyo-max <kyosuke2084@gmail.com>
2021-02-11 22:46:03 +0900

    rubocop実施

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
     if set_user
       redirect_to root_path
     else
-    @order_address = OrderAddress.new
+      @order_address = OrderAddress.new
     end
   end
 
@@ -21,24 +21,23 @@ class OrdersController < ApplicationController
     end
   end
 
-
   private
 
   def order_params
     params.permit(
-      :postal_code,:prefecture_id,:city,:house_number,:building_name,
-      :phone_number,:item_id,:user_id,:authenticity_token, :commit, :price,  :number, :exp_month, :exp_year, :cvc, :token
+      :postal_code, :prefecture_id, :city, :house_number, :building_name,
+      :phone_number, :item_id, :user_id, :authenticity_token, :commit, :price, :number, :exp_month, :exp_year, :cvc, :token
     )
-    .merge(user_id: current_user.id, token: params[:token])
+          .merge(user_id: current_user.id, token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def set_item
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -25,17 +25,17 @@ RSpec.describe OrderAddress, type: :model do
       it '郵便番号にハイフンが含まれていない場合できない' do
         @order_address.postal_code = '1234567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid')
       end
       it '郵便番号が全角では場合できない' do
         @order_address.postal_code = '１２３−４５６７'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid')
       end
       it '都道府県が---の場合保存できない' do
         @order_address.prefecture_id = 1
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@order_address.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it '市区町村が空の場合は保存できない' do
         @order_address.city = nil
@@ -45,7 +45,7 @@ RSpec.describe OrderAddress, type: :model do
       it '市区町村が半角の場合は保存できない' do
         @order_address.city = 'ﾄｳｷｮｳﾄ'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("City is invalid")
+        expect(@order_address.errors.full_messages).to include('City is invalid')
       end
       it '番地が空の場合は保存できない' do
         @order_address.house_number = nil
@@ -53,14 +53,14 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include("House number can't be blank")
       end
       it '電話番号が空の場合は保存できない' do
-      @order_address.phone_number = nil
+        @order_address.phone_number = nil
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
       it '電話番号が全角では保存できない' do
         @order_address.phone_number = '０００１１１１２２２２'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
       it 'tokenが空では保存できない' do
         @order_address.token = nil
@@ -70,12 +70,12 @@ RSpec.describe OrderAddress, type: :model do
       it '電話番号がカタカナでは保存できない' do
         @order_address.phone_number = 'ｾﾞﾛ'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
       it '電話番号が漢字では保存できない' do
         @order_address.phone_number = '零九零'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
     end
   end
